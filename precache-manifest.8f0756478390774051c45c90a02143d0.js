946ac315c051886e0ed4ce5d63d73735e157b065
Dawid Pniewski <dawidpniewski@gmail.com>
2020-10-16 14:03:41 +0200

    Updates

---
+++ precache-manifest.8f0756478390774051c45c90a02143d0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cf5204c870266547ae8",
+    "url": "/app/static/css/main.ceed1a9f.chunk.css"
+  },
+  {
+    "revision": "7cf5204c870266547ae8",
+    "url": "/app/static/js/main.a63e147f.chunk.js"
+  },
+  {
+    "revision": "127917d16c2343d0c8fa",
+    "url": "/app/static/js/runtime~main.d465ad9e.js"
+  },
+  {
+    "revision": "da57f2d152fa96ea8896",
+    "url": "/app/static/js/2.ce94961b.chunk.js"
+  },
+  {
+    "revision": "7a754c5929cd3dd83e593e97e3bf79a8",
+    "url": "/app/static/media/1.7a754c59.jpg"
+  },
+  {
+    "revision": "a607e1990a7e79c8a363af52b6b66f50",
+    "url": "/app/static/media/2.a607e199.jpg"
+  },
+  {
+    "revision": "0ac26e55b6617981b37d9833bbf56ac0",
+    "url": "/app/static/media/3.0ac26e55.jpg"
+  },
+  {
+    "revision": "516d7e9b6521428d34c1f4dea59b7315",
+    "url": "/app/static/media/4.516d7e9b.jpg"
+  },
+  {
+    "revision": "1f2c048263c06c74069f6cc8f38b00d6",
+    "url": "/app/static/media/5.1f2c0482.jpg"
+  },
+  {
+    "revision": "af04fabad19d6420e082d46f45285217",
+    "url": "/app/index.html"
+  }
+];
